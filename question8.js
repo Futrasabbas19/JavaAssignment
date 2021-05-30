7abc1f9a6637857ca30e8beabb5123d161021453
Futras Abbas <82707986+Futrasabbas19@users.noreply.github.com>
2021-05-30 23:23:45 +0500

    8 added

---
+++ question8.js
@@ -0,0 +1,8 @@
+let firstNumber = 0;
+let secondNumber = 100;
+for (firstNumber; firstNumber < secondNumber - 1; ) {
+  firstNumber++;
+  if (firstNumber % 4 === 0) {
+    console.log(firstNumber);
+  }
+}
